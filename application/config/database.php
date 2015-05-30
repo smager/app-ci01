934ed16549c22d379a32ea61726fa6b259cdd7fe
German M. Fuentes <gm_fuentes@yahoo.com>
2015-05-30 20:19:16 +0800

    change database config

--- application/config/database.php
+++ application/config/database.php
@@ -64,10 +64,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'zsi_app',
+	'hostname' => 'oracleexpertscebu.com',
+	'username' => 'zsi',
+	'password' => 'lyncor@tan',
+	'database' => 'zsi_sims',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
