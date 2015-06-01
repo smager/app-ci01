a335febde497d0c6b7983291e087fe38f81eb461
German M. Fuentes <gm_fuentes@yahoo.com>
2015-06-01 22:31:59 +0800

    update front layout

--- application/helpers/include_helper.php
+++ application/helpers/include_helper.php
@@ -102,7 +102,7 @@ function menu(){
 <div class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="../" class="navbar-brand">LynCor.biz</a>
+          <a href="../" class="navbar-brand">LynCor</a>
           <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,52 +8,56 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <?php
     includeHeader();    
 ?> 
-    
+<style>
+.login-panel h1,.login-panel h2{
+      text-align: center;color: #3273AB    
+}    
+.login-panel h2{
+    font-size:14px;
+}    
+.login-form{
+    background-color: #f5f6f6;
+    border: 1pt solid #3071a8;
+    margin: 30px auto 0;
+    min-height: 127px;
+    padding-top: 19px;
+    width: 300px;
+}    
+</style>    
 </head>
 <body>
 <?php menu(); ?> 
     
 <div class="container page">
-
-	<h1>Welcome to Sales Inventory Monitoring System</h1>
-
-    <div class="bswform">  </div>
-    
+    <div class="login-panel col-md-12">
+        <h1>LynCor  </h1>
+        <h2>Sales Inventory Monitoring System</h2>
+        <div class="login-form">  </div>
+    </div>
 </div>
 <script>
 var w = new zsi.bsWriter({
          hasNoConfigFile:false
-	     ,url:"../assets/templates/config.txt"
-		,targetClass:"bswform"
+	     ,url:"<?php echo base_url("config"); ?>"
+		,targetClass:"login-form"
 		,SizeType:"xs"
 });
 
 w.write(function(){	
 	this.div({class:"form-horizontal"})
 		.div({class:"form-group"})
-			.bsLabelInput({labelsize:2,caption:"Login Name",inputsize:3,name:"p_name"})
+			.bsLabelInput({labelsize:4,caption:"Login Name",inputsize:7,name:"p_name"})
 		.div({class:"form-group",parentClass:"form-horizontal"})
-			.bsLabelInput({labelsize:2,caption:"Password",inputsize:3,name:"p_pwd",type:"password"})
-		.div({class:"col-xs-12 buttonGroup",parentClass:"form-horizontal"})
-            .bsButton({id:"btnSave", type:"submit", class:"btn-primary btn-sm",caption:"Save",classicon:"glyphicon-floppy-disk" })	
-            .bsButton({id:"btnClose", type:"button", class:"btn-primary btn-sm",caption:"Close",classicon:"glyphicon-off" })	
+			.bsLabelInput({labelsize:4,caption:"Password",inputsize:7,name:"p_pwd",type:"password"})
+		.div({class:"buttonGroup col-xs-offset-4",parentClass:"form-horizontal"})
+            .bsButton({id:"btnLogin", type:"submit", class:"btn-primary btn-sm",caption:"Login",classicon:"glyphicon glyphicon-log-in" })	
     
 	.end();	
 
 });
-		 
  
-
-function test(){
-    
-console.log( zsi.bsWriter); 
- 
-}    
-    
 </script>        
  
- <input type="button" onclick="test();" value="test" >   
-
  <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>     
 </body>
 </html>
