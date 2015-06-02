7004d1b347e8805d38e661a346ddda06c46e1c8a
gtabinas <gtabinas@gmail.com>
2015-06-02 10:57:03 -0400

    New

---
+++ application/models/Roles_model.php
@@ -0,0 +1,57 @@
+<?php
+class roles_model extends CI_Model{
+    function __construct() {
+        parent::__construct();
+
+    }
+    
+    function getdata(){
+        $query = $this->db->query("SELECT * FROM roles");
+        return $query;    
+    }
+    
+    function update($post){
+    //print_r($post);
+        
+        for ($x = 0; $x < sizeof($post['p_role_desc']); $x++) {
+            $id = $post['p_role_id'][$x];
+            $role_desc = $post['p_role_desc'][$x];
+            
+            if($role_desc !='') {        
+
+                $data = array(
+                     'role_desc' => $role_desc
+
+                );
+                
+                
+                
+                if($id==''){
+                    //insert        
+                    $data['created_by'] =1;
+                    $this->db->set('created_date', 'NOW()', FALSE);
+                    $this->db->insert('roles', $data);
+
+                }else{
+                    //update                        
+                    $data['updated_by'] =1;
+                    $this->db->set('updated_date', 'NOW()', FALSE);
+                    $this->db->where('role_id', $id);
+                    $this->db->update('roles', $data);
+                } 
+                
+            }//end of no data 
+            
+            
+            
+     } //end of loop
+
+    }        
+    
+    function delete($post){        
+        $this->load->model('common_model'); 
+        $this->common_model->delete($this->input->post(),"roles","role_id");        
+    }    
+
+}
+?>
