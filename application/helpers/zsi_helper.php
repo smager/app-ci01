4a31ec348e264846ecb7223dd00d42dd841cf56f
German M. Fuentes <gm_fuentes@yahoo.com>
2015-06-03 18:29:53 +0800

    update bs library

--- application/helpers/zsi_helper.php
+++ application/helpers/zsi_helper.php
@@ -78,12 +78,30 @@ if ( ! function_exists('inputTextBox'))
 
         if (isset($p["attributes"] ) )  $l_attributes =' ' . $p["attributes"];
 
-        if (isset($p["labelName"])) $l_label='  <label class="' . $p["labelSize"] .' control-label" >' . $p["labelName"] . '</label>';
+        if (isset($p["labelName"])) $l_label='  <label class="' . $l_labelSize .' control-label" >' . $p["labelName"] . '</label>';
 
-        echo '<input ' . $l_type . ' class="form-control input-sm ' .  $l_inputclass .  '" id="p_' . $p["name"] . '" name="p_' . $p["name"] . '" ' . $l_inputValue . $l_placeholder . $l_attributes . '>';
+        echo $l_label;
+        if(isset($p["labelName"])) echo "<div  class='$l_inputSize'>";        
+        echo '<input ' . $l_type . ' class="form-control input-sm ' .  $l_inputclass .  '" id="p_' . $p["name"] . '" name="p_' . $p["name"] . '" ' . $l_inputValue . $l_placeholder . $l_attributes . '>';        
+        if(isset($p["labelName"])) echo "</div>";
+    
+    }
+}
 
 
-            
+if ( ! function_exists('OpenFormGroup'))
+{    
+    function OpenFormGroup($p_additional_class=''   ){
+      $l_additional_class ='';
+      if(isset($p_additional_class)) $l_additional_class = ' ' . $p_additional_class;
+      echo "<div class='form-group $l_additional_class'>";            
+    }
+}
+
+if ( ! function_exists('CloseFormGroup'))
+{    
+    function CloseFormGroup(){
+       echo '</div>';            
     }
 }
 
@@ -109,6 +127,7 @@ if ( ! function_exists('selectBox'))
     $l_attributes='';            
     $l_label='';    
     $l_selectedValue='';
+        
 
     if(isset($p["selectSize"])) $l_selectSize =' col-' . $default_label_size_type . '-' . $p["selectSize"];
 
@@ -120,8 +139,12 @@ if ( ! function_exists('selectBox'))
 
     if(isset($p["selectedValue"])) $l_selectedValue=' selectedvalue="' . $p["selectedValue"] . '"';
     echo $l_label;
-
+        
+    if(isset($p["labelName"])) echo "<div  class='$l_selectSize'>";
+    
     echo ' <select  class="form-control input-sm" id="p_' . $p["name"] . '" name="p_' . $p["name"] . '" ' . $l_selectedValue . ' ' . $l_attributes . '></select>';
+    
+    if(isset($p["labelName"])) echo '</div >';    
             
     }
 }
@@ -161,17 +184,19 @@ if ( ! function_exists('yes_no'))
         }
        
         
-        //if(isset($p["label_size"]) ) $l_label_size =' col-' . $l_default_label_size_type . '-' . p_label_size;
-        //if(isset($p["select_size"]) ) $l_select_size ='col-' . $l_default_label_size_type . '-' . p_select_size;
-        //if(isset($p["label_name"]) ) $l_label ='<label class="' . $l_label_size .' control-label" >' . p_label_name . '</label>';
+        if(isset($p["label_size"]) ) $l_label_size =' col-' . $l_default_label_size_type . '-' . p_label_size;
+        if(isset($p["select_size"]) ) $l_select_size ='col-' . $l_default_label_size_type . '-' . p_select_size;
+        if(isset($p["label_name"]) ) $l_label ='<label class="' . $l_label_size .' control-label" >' . p_label_name . '</label>';
 
         $l_name = $p["name"];
         if(isset($p["attributes"]) )    $l_attributes   =$p["attributes"];
 
-        
-        echo '<select class="form-control input-sm" id="p_' . $l_name . '" name="p_' . $l_name . '" ' . $l_attributes . '>';
+                    
+        if(isset($p["labelName"]))  echo '<label class="' . $l_label_size .' control-label" >' . $p["labelName"] . '</label>';
 
-        
+        if(isset($p["labelName"])) echo "<div class='$l_select_size'>";
+      
+        echo '<select class="form-control input-sm" id="p_' . $l_name . '" name="p_' . $l_name . '" ' . $l_attributes . '>';
         
         if(isset($p["mandatory"]) ){
             if( $p["mandatory"]=='N' ) echo '<option value=""></option>';
@@ -191,7 +216,8 @@ if ( ! function_exists('yes_no'))
         }
 
         echo '</SELECT>';
-       
+        if(isset($p["labelName"])) echo "</div>";
+
             
     }
 }
