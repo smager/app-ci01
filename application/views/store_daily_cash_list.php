98b7af8bca865cd19570f48c9dbecb65cff245c4
German M. Fuentes <gm_fuentes@yahoo.com>
2015-06-03 18:46:55 +0800

    remove delete js function

--- application/views/store_daily_cash_list.php
+++ application/views/store_daily_cash_list.php
@@ -60,26 +60,6 @@ $(document).ready(function(){
  ctrlSel( base_url + "common/get_select_data","select[name='p_store_loc_id']","","N","store_loc","store_loc_id","store_loc","");
 });    
      
-function checkDelete(l_cmd) {
-   var l_stmt=[], l_count;
-    
-   var data = zsi.table.getCheckBoxesValues("input[name='p_cb[]']:checked");
-    for(var x=0;x<data.length; x++){
-        l_stmt.push( { name:"p_del_id[]",value : data[x] }  ); 
-    }
-   if (l_stmt!="") {
-      if(confirm("Are you sure you want to delete selected items?")) {
-      $.post( l_cmd , l_stmt, function(d){
-            window.location.reload();
-            //console.log(d);
-         }).fail(function(d) {
-            alert("Sorry, the curent transaction is not successfull.");
-        });
-      }
-   }
-return false;
-}   
-    
 </script>    
 <script type="text/javascript" src="/assets/js/bootstrap-datepicker.js"></script>    
 <?php includeFooter(); ?>    
