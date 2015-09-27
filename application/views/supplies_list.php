1a0849a61795d704df98d3e0897edf09f8e6ee63
gmfuentes <gm_fuentes@yahoo.com>
2015-09-27 09:24:46 -0400

    removed redundant script

--- application/views/supplies_list.php
+++ application/views/supplies_list.php
@@ -17,7 +17,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <?php includePageTemplate(); ?>             
 </div>
 <?php includeHandleBars(); ?>  
-<?php includeHandleBars(); ?>     
 <?php includePageJS(); ?>      
 <?php includeFooter(); ?>     
 </body>
