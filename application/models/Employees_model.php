16cc526b70900e5ff5752e6682afa36e31c42aa1
gtabinas <gtabinas@gmail.com>
2015-10-01 03:08:18 -0400

    Added empl_idno

--- application/models/Employees_model.php
+++ application/models/Employees_model.php
@@ -4,53 +4,54 @@ class employees_model extends CI_Model{
         parent::__construct();
 
     }
-    
+
     function getdata(){
         $query = $this->db->query("SELECT * FROM employees");
-        return $query;    
+        return $query;
     }
-    
+
     function update($post){
     //print_r($post);
-        
+
         for ($x = 0; $x < sizeof($post['p_empl_id']); $x++) {
             $id = $post['p_empl_id'][$x];
-            
+
             $empl_name = $post['p_empl_name'][$x];
-            
-            if($empl_name !='') {        
+
+            if($empl_name !='') {
 
                 $data = array(
                      'empl_name' =>  $empl_name
+                    ,'empl_idno '=>  $post['p_empl_idno'][$x]
                     ,'position_id '=>  $post['p_position_id'][$x]
                     ,'daily_rate'=>  $post['p_daily_rate'][$x]
                     ,'loc_id'=>  $post['p_loc_id'][$x]
                     ,'store_loc_id' =>  $post['p_store_loc_id'][$x]
-                    ,'active'    =>  $post['p_active'][$x]                
-                );                
+                    ,'active'    =>  $post['p_active'][$x]
+                );
                 if($id==''){
-                    //insert        
+                    //insert
                     $data['created_by'] =current_user()->empl_id;
                     $this->db->set('created_date', 'NOW()', FALSE);
                     $this->db->insert('employees', $data);
 
                 }else{
-                    //update                        
+                    //update
                     $data['updated_by'] =current_user()->empl_id;
                     $this->db->set('updated_date', 'NOW()', FALSE);
                     $this->db->where('empl_id', $id);
                     $this->db->update('employees', $data);
-                } 
-                
-            }//end of no data 
+                }
+
+            }//end of no data
 
      } //end of loop
 
-    }        
-    
-    function delete($post){        
-        $this->common_model->delete($this->input->post(),"employees","empl_id");        
-    }    
+    }
+
+    function delete($post){
+        $this->common_model->delete($this->input->post(),"employees","empl_id");
+    }
 
 }
 ?>
